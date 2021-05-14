ddc6e76f4c9550063c2ed73d1e95906365f3d644
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-14 21:19:55 +0100

    Responsive collapsible navbar working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 // Sets the "active" class on the appropriate navigation link item
 function Nav() {
+  const [collapsible, setCollapsible] = useState(true);
+  const handleCollapsible = () => setCollapsible(!collapsible);
   return (
     <nav className="navbar navbar-expand-md bg-dark navbar-dark">
       <Link className="navbar-brand" to="/">
         Alisha Saleh
       </Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"  aria-expanded={!collapsible ? true : false} aria-label="Toggle navigation" onClick={handleCollapsible}>
             <span className="navbar-toggler-icon"></span>
         </button>
-      <div>
+      <div className={`${collapsible ? 'collapse' : ''} navbar-collapse`} id="collapsibleNavbar">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link
