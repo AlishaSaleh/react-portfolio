6039c95588b016660c478b8d53ed75629d73f72f
Alisha <74827217+AlishaSaleh@users.noreply.github.com>
2024-05-04 15:40:48 +0100

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,9 +10,9 @@ function AboutMe() {
                     <h1>About Me</h1>
 
                     <p className="p-3">
-                        I'm a 25 year-old graduate with a passion for all things digital and visual.
+                        I'm a 28 year-old professional with a passion for all things digital and visual.
                         I graduated with a degree in History from the University of Birmingham in 2017 during which I
-                        spent a year abroad at the University of Malta. <br/><br/>
+                        spent a year at the University of Malta. <br/><br/>
 
                         I currently work in the heritage sector and I'm keen to explore ways of engaging a wider range of audiences 
                         with heritage using digital tools and outreach. 
@@ -25,7 +25,7 @@ function AboutMe() {
                         included on my portfolio
                         page! <br/><br/>
 
-                        <a id="cv-ref" href="https://docs.google.com/document/d/10GrT73-yBTW7Vf25WNqfnIDImRAYSESd7MNeAWs63_g/edit?usp=sharing">Click here to view my CV!</a>
+                        // <a id="cv-ref" href="https://docs.google.com/document/d/10GrT73-yBTW7Vf25WNqfnIDImRAYSESd7MNeAWs63_g/edit?usp=sharing">Click here to view my CV!</a>
 
                     </p>
 
